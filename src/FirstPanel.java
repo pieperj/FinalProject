af46174611c0c7fa9467e69e5c090ec9f6653366
Daniel Risher <risherd@student.psdr3.org>
2018-04-24 12:04:44 -0500

    Added Score

--- src/FirstPanel.java
+++ src/FirstPanel.java
@@ -1,12 +1,19 @@
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class FirstPanel extends JPanel {
-
+	
+	private int score = 0;
 	/**
 	 * Create the panel.
 	 */
 	public FirstPanel() {
+		setLayout(null);
+		
+		JLabel scoreLabel = new JLabel("Score: " + score);
+		scoreLabel.setBounds(6, 6, 95, 34);
+		add(scoreLabel);
 
 	}
-
 }
