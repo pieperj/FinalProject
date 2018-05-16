d3fa780ac7e962de5c3f0f62dc24a1be1e203721
Daniel Risher <risherd@student.psdr3.org>
2018-05-16 09:39:28 -0500

    Commit

---
+++ src/Customize.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+
+public class Customize extends JPanel {
+
+	private PanelChangeListener listener;
+	public Customize(PanelChangeListener l) {
+			this.listener = l;
+	}
+
+}
